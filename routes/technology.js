5880cec8435bdc9c9ad2c90dbf5da4053fcd7654
LWalk12 <S551736@nwmissouri.edu>
2026-04-22 17:54:33 -0500

    Part 4: added endpoint authenitcation

--- routes/technology.js
+++ routes/technology.js
@@ -2,13 +2,19 @@ var express = require('express');
 const technology_controlers = require('../controllers/technology');
 var router = express.Router();
 
+const secured = (req,res,next) => {
+    if (req.user){
+        return next()
+    }
+    res.redirect("/login")
+}
 
 /* GET home page. */
 // get technology
 router.get('/', technology_controlers.technology_view_all_Page);
 router.get('/detail', technology_controlers.technology_view_one_Page);
 router.get('/create', technology_controlers.technology_create_Page);
-router.get('/update', technology_controlers.technology_update_Page);
+router.get('/update',secured, technology_controlers.technology_update_Page);
 router.get('/delete', technology_controlers.technology_delete_Page);
 router.get('/:id', technology_controlers.technology_detail);
 module.exports = router;
