6a4691b6ac963db8ef533435b258842e9f7aaf7d
LWalk12 <S551736@nwmissouri.edu>
2026-04-22 17:35:29 -0500

    part3: added login pages

--- routes/technology.js
+++ routes/technology.js
@@ -2,6 +2,7 @@ var express = require('express');
 const technology_controlers = require('../controllers/technology');
 var router = express.Router();
 
+
 /* GET home page. */
 // get technology
 router.get('/', technology_controlers.technology_view_all_Page);
